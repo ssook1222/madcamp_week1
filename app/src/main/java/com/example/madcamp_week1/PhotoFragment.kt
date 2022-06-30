15871af24667721a5ff94d2a630967505491c724
ssook1222 <chlthr1222@naver.com>
2022-06-30 17:57:33 +0900

    UPDATE : 이미지 추가 및 기본 썸네일 초기 설정

--- app/src/main/java/com/example/madcamp_week1/PhotoFragment.kt
+++ app/src/main/java/com/example/madcamp_week1/PhotoFragment.kt
@@ -7,13 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONArray
+import org.json.JSONTokener
 
 class PhotoFragment : Fragment() {
     lateinit var recyclerView : RecyclerView
-
-    var photosList = arrayListOf<Photos>(
-        Photos("ssook",R.drawable.box)
-    )
+    var photosList = arrayListOf<Photos>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +20,18 @@ class PhotoFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_photo, container, false)
         val contactAdapter = PhotoAdapter(photosList)
+
+        if (photosList.isEmpty()) {
+            val contactsJsonString: String = requireActivity().assets.open("contacts.json").bufferedReader().use {
+                it.readText()
+            }
+            val photoJsonArray = JSONTokener(contactsJsonString).nextValue() as JSONArray
+
+            for (i in 0 until photoJsonArray.length()) {
+                val name = photoJsonArray.getJSONObject(i).getString("name")
+                photosList.add(Photos(name, R.drawable.sonagi_logo))
+            }
+        }
 
         recyclerView = rootView.findViewById(R.id.photoRecyclerView!!)as RecyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
