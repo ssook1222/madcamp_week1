bcdc940315a56c1330157c37435a959a8eccf276
ssook1222 <chlthr1222@naver.com>
2022-07-02 15:15:12 +0900

    UPDATE : 탭3 UI 수정

--- app/src/main/java/com/example/madcamp_week1/ShowDiaryActivity.kt
+++ app/src/main/java/com/example/madcamp_week1/ShowDiaryActivity.kt
@@ -9,6 +9,7 @@ class ShowDiaryActivity : AppCompatActivity() {
     private lateinit var diaryTitleView: TextView
     private lateinit var diaryTextView: TextView
     private lateinit var diaryImageView: ImageView
+    private lateinit var diaryDateView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +18,7 @@ class ShowDiaryActivity : AppCompatActivity() {
         diaryTitleView = findViewById<TextView>(R.id.diaryTitleView)
         diaryTextView = findViewById<TextView>(R.id.diaryTextView)
         diaryImageView = findViewById<ImageView>(R.id.diaryImageView)
+        diaryDateView = findViewById<TextView>(R.id.diaryDateView)
 
         // diaryData is String[] { date, name, resId.toString(), title }
         val diaryData = intent.getStringArrayExtra("diaryData") as Array<String>
@@ -26,6 +28,7 @@ class ShowDiaryActivity : AppCompatActivity() {
 
         diaryTitleView?.text = diaryData[3]
         diaryImageView?.setImageResource(diaryData[2].toInt())
+        diaryDateView?.text = diaryData[0]
         diaryTextView?.text = diaryText
     }
 }
